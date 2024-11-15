a775450901a334de624dd31b70869590bfbef4fb
Jason Tran <jasonhvtran@outlook.com>
2024-11-15 10:07:36 -0700

    Update questions

--- src/test/VersionControl/VC_Test.cpp
+++ src/test/VersionControl/VC_Test.cpp
@@ -10,47 +10,37 @@
 #include "oatpp/web/client/HttpRequestExecutor.hpp"
 
 void VC_Test::onRun() {
-  /* Register test components */
   TestComponent component;
 
-  /* Create client-server test runner */
   oatpp::test::web::ClientServerTestRunner runner;
 
-  /* Add VCController endpoints to the router of the test server */
   runner.addController(std::make_shared<VCController>());
 
-  /* Run test */
   runner.run(
       [this, &runner] {
-        /* Get client connection provider for Api Client */
         OATPP_COMPONENT(std::shared_ptr<oatpp::network::ClientConnectionProvider>, clientConnectionProvider);
-
-        /* Get object mapper component */
         OATPP_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, objectMapper);
 
-        /* Create http request executor for Api Client */
         auto requestExecutor =
             oatpp::web::client::HttpRequestExecutor::createShared(clientConnectionProvider);
 
-        /* Create Test API client */
         auto client = MyApiTestClient::createShared(requestExecutor, objectMapper);
 
-        /* Call server API */
         auto response = client->getVCQuestion();
-
-        /* Assert that server responds with 200 */
         OATPP_ASSERT(response->getStatusCode() == 200);
 
-        /* Read response body as VCResult_MC */
-        auto message =
-            response->readBodyToDto<oatpp::Object<VCResult_MC>>(objectMapper.get());
+        auto message = response->readBodyToDto<oatpp::Object<VCResult_MC>>(objectMapper.get());
 
-        /* Assert that received message is as expected */
         OATPP_ASSERT(message);
-        OATPP_ASSERT(message->questionText == "What is version control?");
-      },
-      std::chrono::minutes(10) /* test timeout */);
+        OATPP_ASSERT(message->questionText == "Which command is used to initialize a Git repository?");
 
-  /* wait all server threads finished */
+        // Ensure options are correct
+        OATPP_ASSERT(message->optionA == "git init");
+        OATPP_ASSERT(message->optionB == "git start");
+        OATPP_ASSERT(message->optionC == "git begin");
+        OATPP_ASSERT(message->optionD == "git create");
+      },
+      std::chrono::minutes(1) /* test timeout */);
+
   std::this_thread::sleep_for(std::chrono::seconds(1));
 }
