bfa801d76925978936260422d23741190068ae16
Jason Tran <jasonhvtran@outlook.com>
2024-11-15 13:04:42 -0700

    Updated DP_Test to keep OATPP_Componenet calls within lambda

--- src/test/DesignPatterns/DP_Test.cpp
+++ src/test/DesignPatterns/DP_Test.cpp
@@ -16,38 +16,41 @@ void DP_Test::onRun() {
   // Create client-server test runner
   oatpp::test::web::ClientServerTestRunner runner;
 
-  // Get object mapper component
-  OATPP_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, objectMapper);
-
   // Add DPController endpoints to the router of the test server
   runner.addController(std::make_shared<DPController>());
 
   // Run test
-  runner.run([this, &runner] {
-    // Get client connection provider for Api Client
-    OATPP_COMPONENT(std::shared_ptr<oatpp::network::ClientConnectionProvider>, clientConnectionProvider);
+  runner.run(
+      [this, &runner] {  // Keep this format, capture `runner`
+        // Get client connection provider for Api Client
+        OATPP_COMPONENT(std::shared_ptr<oatpp::network::ClientConnectionProvider>, clientConnectionProvider);
 
-    // Create Test API client
-    auto client = MyApiTestClient::createShared(
-      oatpp::web::client::HttpRequestExecutor::createShared(clientConnectionProvider),
-      objectMapper
-    );
+        // Get object mapper component
+        OATPP_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, objectMapper);
 
-    // Test 200 Success
-    auto response = client->getDPQuestion();
-    OATPP_ASSERT(response->getStatusCode() == 200);
+        // Create http request executor for Api Client
+        auto requestExecutor =
+            oatpp::web::client::HttpRequestExecutor::createShared(clientConnectionProvider);
 
-    // Read response body as DPResult_MC DTO
-    auto message = response->readBodyToDto<oatpp::Object<DPResult_MC>>(objectMapper.get());
+        // Create Test API client
+        auto client = MyApiTestClient::createShared(requestExecutor, objectMapper);
 
-    // Assert that received message is as expected
-    OATPP_ASSERT(message);
-    OATPP_ASSERT(message->questionText == "Which design pattern ensures a class has only one instance and provides a global point of access to it?");
-    OATPP_ASSERT(message->optionA == "Factory Pattern");
-    OATPP_ASSERT(message->optionB == "Singleton Pattern");
-    OATPP_ASSERT(message->optionC == "Observer Pattern");
-    OATPP_ASSERT(message->optionD == "Strategy Pattern");
-  }, std::chrono::minutes(10));
+        // Call server API
+        auto response = client->getDPQuestion();
+        OATPP_ASSERT(response->getStatusCode() == 200);
+
+        // Read response body as DPResult_MC DTO
+        auto message = response->readBodyToDto<oatpp::Object<DPResult_MC>>(objectMapper.get());
+
+        // Assert that received message is as expected
+        OATPP_ASSERT(message);
+        OATPP_ASSERT(message->questionText == "Which design pattern ensures a class has only one instance and provides a global point of access to it?");
+        OATPP_ASSERT(message->optionA == "Factory Pattern");
+        OATPP_ASSERT(message->optionB == "Singleton Pattern");
+        OATPP_ASSERT(message->optionC == "Observer Pattern");
+        OATPP_ASSERT(message->optionD == "Strategy Pattern");
+      },
+      std::chrono::minutes(10) /* test timeout */);
 
   // Wait for all server threads to finish
   std::this_thread::sleep_for(std::chrono::seconds(1));
