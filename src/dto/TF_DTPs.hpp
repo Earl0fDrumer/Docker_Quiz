60ed4bc3394922f2f2a977fa1174077eedbe0740
blap3720 <p.blach@uleth.ca>
2024-11-22 18:18:12 -0700

    Added TF stuff

---
+++ src/dto/TF_DTPs.hpp
@@ -0,0 +1,19 @@
+#ifndef TF_DTO_hpp
+#define TF_DTO_hpp
+
+#include "oatpp/core/Types.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+class Result_TF : public oatpp::DTO {
+  DTO_INIT(Result_MC, DTO /* Extends */)
+
+  DTO_FIELD(String, questionText);
+  DTO_FIELD(String, trueText);
+  DTO_FIELD(String, falseText);
+};
+
+#include OATPP_CODEGEN_END(DTO)
+
+#endif /* TF_DTO_hpp */
